2632ac5b326a7369349b587e5976c191bde17d5f
Bartholomew de La Villardière <exabot@gmail.com>
2016-05-19 22:56:35 +0200

    add some comments

--- example/xor.ml
+++ example/xor.ml
@@ -19,9 +19,11 @@ begin
 
     (* We use the make_layered_network helper function that allows to easily make a multi layered network *)
     let network = make_layered_network [2; 4; 1] in
+    (* The network will contain 2 input neurons (the neurons 0 and 1) and 1 output neuron (the neuron 6) *)
 
     (* LEARN MY LITTLE BRAIN! *)
     for i = 0 to 500 do
+        (* [0; 1] are the id of input neurons and [6] the id of the output neuron *)
         train network trainset [0; 1] [6];
     done;
 
