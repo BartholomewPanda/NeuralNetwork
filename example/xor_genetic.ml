d9d5e88dd199bc6b138cc3cff80eda8ce046a3f6
Bartholomew de La Villardière <exabot@gmail.com>
2016-05-22 23:36:50 +0200

    code refactoring + comments

--- example/xor_genetic.ml
+++ example/xor_genetic.ml
@@ -11,61 +11,28 @@ let trainset =
         [1.0; 1.0], 0.0
     ]
 
-let randomize indiv =
-    let randomize_weights neuron =
-        Hashtbl.iter (fun id _ -> Hashtbl.replace neuron.weights id (Utils.random_uniform 1.0)) neuron.weights
-    in
-    Hashtbl.iter (fun _ n -> randomize_weights n) indiv.neurons
-
-let fitness indiv =
-    trainset
-    |> List.map (fun (inputs, target) ->
-        let _, result = List.hd (feedforward indiv [0; 1] inputs) in
-        abs_float (result -. target))
-    |> List.sort (fun f1 f2 -> compare f1 f2 * -1)
-    |> List.hd
-
-let select population_size population =
-    population
-    |> Utils.list_take (population_size / 2)
-    |> List.map (fun (indiv, _) -> indiv)
-
-let breeding mother father =
-    let c1, c2 = copy_network mother, copy_network father in
-    let point  = Random.int (Hashtbl.length mother.neurons) in
-    let rec switch_neurons i =
-        if i < point then
-        begin
-            Hashtbl.replace c1.neurons i (Hashtbl.find father.neurons i);
-            Hashtbl.replace c2.neurons i (Hashtbl.find mother.neurons i);
-        end
-    in
-    switch_neurons 0;
-    (c1, c2)
-
-let mutation indiv =
-    let mutate_weights neuron input value =
-        if Random.int 3 = 0 then
-            Hashtbl.replace neuron.weights input (Utils.random_uniform 10.0 +. value)
-    in
-    let mutate_neuron neuron =
-        Hashtbl.iter (mutate_weights neuron) neuron.weights
-    in
-    Hashtbl.iter (fun _ n -> mutate_neuron n) indiv.neurons
-
-let finished population =
-    let indiv, fitness = List.hd population in
+(* TODO: find an other way and remove this ugly hack... *)
+let debug f population =
+    let _, fitness = List.hd population in
     Printf.printf "fitness: %f\n" fitness;
     flush stdout;
-    fitness < 0.01
-
+    f population
 
 let test =
 begin
     Random.self_init ();
 
     let network    = make_layered_network [2; 4; 1] in
-    let population = train network randomize fitness select breeding mutation finished 100 in
+    let population =
+        train network
+            Randomizer.weights_randomizer
+            (Fitness.simple_fitness trainset)
+            Selection.simple_select
+            Breeding.one_point_crossover
+            (Mutation.weights_random_mutation 0.3)
+            (debug (Finished.finished 0.000001))
+            100
+    in
     let best, _    = List.hd population in
 
     Printf.printf "XOR function computed by the best individual:\n";
--- genetic.ml
+++ genetic.ml
@@ -2,6 +2,116 @@
 open NeuralNetwork
 
 
+(** Genetic operators to randomize individuals. *)
+module Randomizer =
+struct
+
+    (** Randomize each weights of the network. *)
+    let weights_randomizer indiv =
+        let randomize_weights neuron =
+            Hashtbl.iter
+                (fun id _ -> Hashtbl.replace neuron.weights id (Utils.random_uniform 1.0))
+                neuron.weights
+        in
+        Hashtbl.iter (fun _ n -> randomize_weights n) indiv.neurons
+
+    (** TODO: topology randomizer, topology + weights randomizer *)
+
+end
+
+
+(** Genetic operators to compute the fitness of individuals. *)
+module Fitness =
+struct
+
+    (** Apply the trainset, compute the errors for each entry of the trainset
+       and return the greatest one. TODO: find an other name to this function. *)
+    let simple_fitness trainset indiv =
+        trainset
+        |> List.map (fun (inputs, target) ->
+            let _, result = List.hd (feedforward indiv [0; 1] inputs) in
+            abs_float (result -. target))
+        |> List.sort (fun f1 f2 -> compare f1 f2 * -1)
+        |> List.hd
+
+    (** TODO: sum of the errors *)
+
+end
+
+
+(** Genetic operators to select individuals for the next population. *)
+module Selection =
+struct
+
+    (** Get the half of the best individuals of the population. *)
+    let simple_select population_size population =
+        population
+        |> Utils.list_take (population_size / 2)
+        |> List.map (fun (indiv, _) -> indiv)
+
+    (** TODO: tournament selection, fitness proportionate selection *)
+
+end
+
+
+(** Genetic operators for the breeding. *)
+module Breeding =
+struct
+
+    (** Simple one point crossover between two individuals. *)
+    let one_point_crossover mother father =
+        let c1, c2 = copy_network mother, copy_network father in
+        let point  = Random.int (Hashtbl.length mother.neurons) in
+        let rec switch_neurons i =
+            if i < point then
+            begin
+                Hashtbl.replace c1.neurons i (Hashtbl.find father.neurons i);
+                Hashtbl.replace c2.neurons i (Hashtbl.find mother.neurons i);
+            end
+        in
+        switch_neurons 0;
+        (c1, c2)
+
+    (** TODO: cut and splice, uniform crossover *)
+
+end
+
+
+(** Genetic operators for mutations. *)
+module Mutation =
+struct
+
+    (** Random mutations on the weights of the network.
+       @param rate the rate of mutation *)
+    let weights_random_mutation rate indiv =
+        let mutate_weights neuron input value =
+            if Random.float 1.0 <= rate then
+                Hashtbl.replace neuron.weights input (Utils.random_uniform 10.0 +. value)
+        in
+        let mutate_neuron neuron =
+            Hashtbl.iter (mutate_weights neuron) neuron.weights
+        in
+        Hashtbl.iter (fun _ n -> mutate_neuron n) indiv.neurons
+
+    (** TODO: random topology mutation, weights + topology mutation *)
+
+end
+
+(** Collection of "finished" function. *)
+module Finished =
+struct
+
+    (** Stop the genetic algorithm when a limit is reached. *)
+    let finished limit population =
+        let _, fitness = List.hd population in
+        fitness <= limit
+
+end
+
+module Population =
+struct
+end
+
 let make_new_population breeding mutation population_size population =
     let rec make parents children nb =
         if nb < population_size then
@@ -36,4 +146,5 @@ let train network randomize fitness select breeding mutation finished population
     let population = Utils.list_apply (fun _ -> copy_network network) population_size in
     List.iter randomize population;
     genetic fitness select breeding mutation finished population_size population
+
 
